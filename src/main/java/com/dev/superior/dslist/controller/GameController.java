23f1ec7ec40aa07703cffcbe26bc6d97359c3965
vitorramires468 <ramiresvitor468@gmail.com>
2025-01-22 19:43:45 -0300

    GEt game by id

--- src/main/java/com/dev/superior/dslist/controller/GameController.java
+++ src/main/java/com/dev/superior/dslist/controller/GameController.java
@@ -1,9 +1,11 @@
 package com.dev.superior.dslist.controller;
 
+import com.dev.superior.dslist.dto.GameDTO;
 import com.dev.superior.dslist.dto.GameMinDTO;
 import com.dev.superior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,5 +22,11 @@ public class GameController {
     public List<GameMinDTO> listarGames(){
         var result = gameService.findAll();
         return result;
+    }
+
+    @GetMapping("/{id}")
+    public GameDTO detalharGame(@PathVariable Long id){
+        var dto = gameService.findById(id);
+        return dto;
     }
 }
--- src/main/java/com/dev/superior/dslist/services/GameService.java
+++ src/main/java/com/dev/superior/dslist/services/GameService.java
@@ -1,11 +1,16 @@
 package com.dev.superior.dslist.services;
 
+import com.dev.superior.dslist.dto.GameDTO;
 import com.dev.superior.dslist.dto.GameMinDTO;
+import com.dev.superior.dslist.entities.Game;
 import com.dev.superior.dslist.repository.GameRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class GameService {
@@ -13,8 +18,20 @@ public class GameService {
     @Autowired
     private GameRepository gameRepository;
 
+    @Transactional(readOnly = true)
     public List<GameMinDTO> findAll(){
         var result = gameRepository.findAll();
         return result.stream().map(g -> new GameMinDTO(g)).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public GameDTO findById(Long id){
+        Optional<Game> optionalGame = gameRepository.findById(id);
+        if(optionalGame.isPresent()){
+            var game = optionalGame.get();
+            return new GameDTO(game);
+        }else{
+            throw new RuntimeException("Registro não cadastrado no banco!");
+        }
     }
 }
